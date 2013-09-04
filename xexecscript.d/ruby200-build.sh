46814ea8a6d1ddad0cf07cb7e38b6d9ce747a303
Masahito Yoshida <masahito@axsh.net>
2013-09-04 12:25:37 +0900

    change version number from 2.0.0pXXX to 2.0.0.XXX.

--- xexecscript.d/ruby200-build.sh
+++ xexecscript.d/ruby200-build.sh
@@ -11,7 +11,7 @@ echo "doing execscript.sh: $1"
 
 ## root
 
-chroot $1 $SHELL <<'EOS'
+chroot $1 $SHELL -ex <<'EOS'
   rpm -Uvh http://dlc.wakame.axsh.jp.s3-website-us-east-1.amazonaws.com/epel-release
   yum install -y libyaml-devel
 EOS
@@ -30,14 +30,14 @@ chroot $1 su - ${devel_user} <<'EOS'
   cp -f /tmp/ruby-2.0.0-rpm/ruby200.spec ~/rpmbuild/SPECS/ruby200.spec
 
   cd ~/rpmbuild/SOURCES; pwd
-  wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-${rubyminorver}.tar.gz
+  wget http://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p${rubyminorver}.tar.gz
 
   rpmbuild -bb ~/rpmbuild/SPECS/ruby200.spec
 EOS
 
 ## root
 
-chroot $1 $SHELL <<EOS
-  rpm -ivh /home/${devel_user}/rpmbuild/RPMS/*/ruby-2.0.0p*.rpm
+chroot $1 $SHELL -ex <<EOS
+  rpm -ivh /home/${devel_user}/rpmbuild/RPMS/*/ruby-2.0.0.*.rpm
   gem install bundler --no-rdoc --no-ri
 EOS
